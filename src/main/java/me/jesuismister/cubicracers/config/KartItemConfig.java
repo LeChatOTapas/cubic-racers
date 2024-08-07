77ee9add4f219ecfb1e9b62c17ec5d9d5a5d85cb
Quentin BERNARD <q.bernard62@outlook.fr>
2024-08-07 16:08:39 +0200

    modif des drops rates par défaut (car personne les modifs) dans les items box

--- src/main/java/me/jesuismister/cubicracers/config/KartItemConfig.java
+++ src/main/java/me/jesuismister/cubicracers/config/KartItemConfig.java
@@ -12,15 +12,23 @@ public class KartItemConfig {
     public static Map<String, ForgeConfigSpec.DoubleValue> ITEMS_DROP_RATES = new HashMap<>();
 
     public static void registerServerConfig(ForgeConfigSpec.Builder SERVER_BUILDER) {
-        List<String> itemList = List.of("Banana", "Green_shell", "Mushroom", "Fake_box", "Bob_omb", "Star", "Thunder", "Klaxon");
+        Map<String, Float> itemList = Map.of(
+                "Banana", 20f,
+                "Green_shell", 30f,
+                "Mushroom", 20f,
+                "Fake_box", 10f,
+                "Bob_omb", 8f,
+                "Star", 3f,
+                "Thunder", 1f,
+                "Klaxon", 8f
+        );
 
         SERVER_BUILDER.comment("Settings for all the drop rates from the item box").push("item_box_drop_rates");
-        for(String item : itemList){
-            SERVER_BUILDER.push(item);
-
-            ITEMS_DROP_RATES.put(item, SERVER_BUILDER
-                    .comment("Drop rate of " + item)
-                    .defineInRange("value", 12.5, 0, 100));
+        for(Map.Entry<String, Float> item : itemList.entrySet()){
+            SERVER_BUILDER.push(item.getKey());
+            ITEMS_DROP_RATES.put(item.getKey(), SERVER_BUILDER
+                    .comment("Drop rate of " + item.getKey())
+                    .defineInRange("value", item.getValue(), 0, 100));
             SERVER_BUILDER.pop();
         }
         SERVER_BUILDER.pop();
